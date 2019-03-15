d7134f8e88b93bd8ca03842be5eb19d84871752d
Cheolkie Kim <ckiekim@naver.com>
2019-03-15 10:50:35 +0900

    Chapter 3

--- ch03/src/sec04/exam01_arithmetic/InfinityAndNaNCheckExample.java
+++ ch03/src/sec04/exam01_arithmetic/InfinityAndNaNCheckExample.java
@@ -12,26 +12,13 @@ public class InfinityAndNaNCheckExample {
 		System.out.println(Double.isNaN(z));	
 		
 		//잘못된 코드
-/*		System.out.println(z + 2);	
+		//System.out.println(z + 2);	
 		
 		//알맞은 코드
 		if(Double.isInfinite(z) || Double.isNaN(z)) { 
 			System.out.println("값 산출 불가"); 
 		} else { 
 			System.out.println(z + 2); 
-		}*/
-		
-		//---------------------------------------------------
-		
-		/*int x = 5;
-		int y = 0;
-		
-		try {
-			//int z = x / y;
-			int z = x % y;
-			System.out.println("z: " + z);
-		} catch(ArithmeticException e) {
-			System.out.println("0으로 나누면 안됨");
-		}*/
+		}
 	}
 }
---
+++ ch03/src/sec04/exam05_bit/BitShiftExample.java
@@ -0,0 +1,16 @@
+package sec04.exam05_bit;
+
+public class BitShiftExample {
+	public static void main(String[] args) {
+		System.out.println("1 << 3 = " + (1<<3));		
+		System.out.println("-8 >> 3 = " + (-8>>3));	
+		System.out.println("-8 >>> 3 = " + (-8>>>3));
+		
+		System.out.println(BitLogicExample.toBinaryString(-8));
+		System.out.println(">> 3");
+		System.out.println(BitLogicExample.toBinaryString(-8 >> 3));
+		System.out.println(">>> 3");
+		System.out.println(BitLogicExample.toBinaryString(-8 >>> 3));
+	}
+}
+
