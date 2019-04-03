cf966bc3bc86ad9e05641f879945420284c2b9c3
Cheolkie Kim <ckiekim@naver.com>
2019-04-03 17:20:43 +0900

    Member DB 연습

--- MySQL/src/basic03/EaglesTest.java
+++ MySQL/src/basic03/EaglesTest.java
@@ -17,8 +17,6 @@ public class EaglesTest {
 		
 		eDao.deletePlayer(6);
 		List<EaglesDTO> playerList = eDao.selectAll();*/
-		/*List<EaglesDTO> playerList = eDao.selectPlayers();
-		System.out.println("연봉 3억원 이상 선수 명단");*/
 		String positions[] = {"투수", "포수", "내야수", "외야수"};
 		for (String position: positions) {
 			List<EaglesDTO> playerList = eDao.selectPlayersByPosition(position);
@@ -27,6 +25,10 @@ public class EaglesTest {
 				System.out.println(eagle.toString());
 			System.out.println();
 		}
+		System.out.println("연봉 2억원 이상 선수 명단");
+		List<EaglesDTO> playerList = eDao.selectPlayersBySalary(200000);
+		for (EaglesDTO eagle: playerList)
+			System.out.println(eagle.toString());
 		
 		eDao.close();
 	}
