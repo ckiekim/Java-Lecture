9050e22a70f3af5358a6341544415f52f515630c
Cheolkie Kim <ckiekim@naver.com>
2019-04-05 17:35:31 +0900

    Chapter 13. Generic

---
+++ ch13/src/sec05/exam01_bounded_type/BoundedTypeParameterExample.java
@@ -0,0 +1,16 @@
+package sec05.exam01_bounded_type;
+
+public class BoundedTypeParameterExample {
+	public static void main(String[] args) {
+		//String str = Util.compare("a", "b"); (x)
+		
+		int result1 = Util.compare(10, 20);
+		System.out.println(result1);
+		
+		int result2 = Util.compare(4.5, 3);
+		System.out.println(result2);
+		
+		int result3 = Util.compare(5.5, 5.5);
+		System.out.println(result3);
+	}
+}
