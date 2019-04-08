1308259f5c9a939fb1e23b063714f5cfc05ddc68
Cheolkie Kim <ckiekim@naver.com>
2019-04-08 12:13:07 +0900

    Chapter 14. Lambda Expression

---
+++ ch14/src/sec04/exam02_local_variables/MyFunctionalInterface.java
@@ -0,0 +1,6 @@
+package sec04.exam02_local_variables;
+
+public interface MyFunctionalInterface {
+    public void method();
+}
+
---
+++ ch14/src/sec04/exam02_local_variables/UsingLocalVariableExample.java
@@ -0,0 +1,9 @@
+package sec04.exam02_local_variables;
+
+public class UsingLocalVariableExample {
+	public static void main(String... args) {
+		UsingLocalVariable ulv = new UsingLocalVariable();
+		ulv.method(20);
+	}
+}
+
