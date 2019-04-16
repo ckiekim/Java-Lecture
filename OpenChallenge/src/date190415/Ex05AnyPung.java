462f3ddce7c00f58012e2014ab1c1ea16c9ba81f
Cheolkie Kim <ckiekim@naver.com>
2019-04-16 16:43:39 +0900

    Solved AnyPung

--- OpenChallenge/src/date190415/Ex05AnyPung.java
+++ OpenChallenge/src/date190415/Ex05AnyPung.java
@@ -8,12 +8,12 @@ public class Ex05AnyPung {
 		for (int i=0; i<5; i++)
 			for (int k=0; k<5; k++)
 				tile[i][k] = (int)(Math.random()*4) + 1;
-		printTile(tile);
 
 		while (true) {
+			printTile(tile);
+			System.out.println();
 			int[][] found = findPung(tile);
-			break;
-/*			boolean check = false;
+			boolean check = false;
 			for (int i=0; i<5; i++) {
 				for (int k=0; k<5; k++)
 					if (found[i][k] == 8) {
@@ -21,10 +21,9 @@ public class Ex05AnyPung {
 						break;
 					}
 			}
-			if (check)
+			if (!check)
 				break;
 			tile = collapsePung(found, tile);
-			printTile(tile);*/
 		}
 	}
 	static int[][] findPung(int[][] src) {
@@ -38,10 +37,6 @@ public class Ex05AnyPung {
 			cols[i] = findRowPung(tpSrc[i]);
 		}
 		int[][] tpCols = transpose(cols);
-		/*System.out.println("Rows:");
-		printTile(rows);
-		System.out.println("Columns:");
-		printTile(tpCols);*/
 		// merge rows and columns
 		for (int i=0; i<5; i++)
 			for (int k=0; k<5; k++)
@@ -49,13 +44,13 @@ public class Ex05AnyPung {
 					dst[i][k] = 8;
 				else
 					dst[i][k] = src[i][k];
-		System.out.println("findPung()");
-		printTile(dst);
+		//System.out.println("findPung()");
+		//printTile(dst);
 		return dst;
 	}
 	static int[] findRowPung(int[] row) {
 		int result[] = new int[5];
-		if (row[0] == row[1] && row[1] == row[2]) {
+		if (row[0] != 0 && row[0] == row[1] && row[1] == row[2]) {
 			if (row[2] == row[3] && row[3] == row[4])
 				for(int i=0; i<5; i++)
 					result[i] = 8;
@@ -68,7 +63,7 @@ public class Ex05AnyPung {
 					result[i] = 8;
 				result[3] = row[3]; result[4] = row[4];
 			}
-		} else if (row[1] == row[2] && row[2] == row[3]) {
+		} else if (row[1] != 0 && row[1] == row[2] && row[2] == row[3]) {
 			result[0] = row[0];
 			if (row[3] == row[4]) 
 				for(int i=1; i<5; i++)
@@ -78,7 +73,7 @@ public class Ex05AnyPung {
 					result[i] = 8;
 				result[4] = row[4];
 			}
-		} else if (row[2] == row[3] && row[3] == row[4]) {
+		} else if (row[2] != 0 && row[2] == row[3] && row[3] == row[4]) {
 			result[0] = row[0]; result[1] = row[1];
 			for(int i=2; i<5; i++)
 				result[i] = 8;
@@ -89,17 +84,47 @@ public class Ex05AnyPung {
 		//System.out.println(Arrays.toString(result));
 		return result;
 	}
+	static int[][] collapsePung(int[][] key, int[][] src) {
+		int[][] dst = new int[5][5];
+		for (int i=0; i<5; i++) {
+			for (int k=0; k<5; k++) {
+				if (key[i][k] == 8) {
+					if (i==0) 
+						src[i][k] = 0;
+					else if (i==1) {
+						src[i][k] = src[i-1][k];
+						src[0][k] = 0;
+					} else if (i==2) {
+						src[i][k] = src[i-1][k];
+						src[i-1][k] = src[i-2][k];
+						src[0][k] = 0;
+					} else if (i==3) {
+						src[i][k] = src[i-1][k];
+						src[i-1][k] = src[i-2][k];
+						src[i-2][k] = src[i-3][k];
+						src[0][k] = 0;
+					} else if (i==4) {
+						src[i][k] = src[i-1][k];
+						src[i-1][k] = src[i-2][k];
+						src[i-2][k] = src[i-3][k];
+						src[i-3][k] = src[i-4][k];
+						src[0][k] = 0;
+					}
+				}
+			}
+		}
+		for (int i=0; i<5; i++)
+			for (int k=0; k<5; k++)
+				dst[i][k] = src[i][k];
+		//System.out.println("collapsePung()");
+		//printTile(dst);
+		return dst;
+	}
 	static int[][] transpose(int[][] src) {
 		int[][] dst = new int[5][5];
 		for (int i=0; i<5; i++)
 			for (int k=0; k<5; k++)
 				dst[k][i] = src[i][k];
-		return dst;
-	}
-	static int[][] collapsePung(int[][] key, int[][] src) {
-		int[][] dst = new int[5][5];
-		
-		
 		return dst;
 	}
 	static void printTile(int[][] tile) {
